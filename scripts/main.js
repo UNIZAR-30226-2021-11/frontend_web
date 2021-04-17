5b9a4452dfbd718c4a245c48837bfe54c21f6862
Mario <mario@MacBook-Pro-de-Mario.local>
2021-04-17 02:49:27 +0200

    Commit de los primeros HTML sin JavaScript

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+let boton1 = document.querySelector("#botonJugarSolo");
+let boton2 = document.querySelector("#botonJugarOnline");
+let boton3 = document.querySelector("#botonPerfil");
+let boton4 = document.querySelector(".engranaje");
+
+boton1.onclick = function() {
+    alert("Comienza el modo de juego en solitario.");
+}
+boton2.onclick = function() {
+    window.location.href = "onlineInicio.html";
+}
+boton3.onclick = function() {
+    alert("Visita al perfil de usuario.");
+}
+boton4.onclick = function() {
+    alert("Botón de opciones pulsado");
+}
