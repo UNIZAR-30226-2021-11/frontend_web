292c451abe48a74d654685f01d613465f88ecf25
Mario <mario@MacBook-Pro-de-Mario.local>
2021-05-27 03:42:12 +0200

    Retoques finales de diseño

--- scripts/listaPartidasTorneo.js
+++ scripts/listaPartidasTorneo.js
@@ -1,4 +1,12 @@
 let token = sessionStorage.getItem('token');
+let json = {};
+
+function joinPartida() {
+    let index = event.target.className;
+    sessionStorage.setItem('idPartida', `${json.games[index].id}`);
+    sessionStorage.setItem('nombrePartida', `${json.games[index].name}`);
+    window.location.href = "salaEquipos.html";
+}
 
 fetch("http://15.188.14.213:11050/api/v1/games/tournament", {
         method: "GET",
@@ -15,19 +23,18 @@ fetch("http://15.188.14.213:11050/api/v1/games/tournament", {
         }
     })
     .then(data => {
-        let json = JSON.parse(data);
+        json = JSON.parse(data);
         let lista = document.querySelector(".listaPartidas");
         if (json.games != null) {
             for (let i = 0; i < json.games.length; i++) {
                 lista.innerHTML += `<tr>
                                     <td>${json.games[i].name}</td>
-                                    <td><button type="button" class="join${i}">Unirse</button>
+                                    <td><button type="button" class="${i}">Unirse</button>
                                 </tr>`;
-                document.querySelector(`.join${i}`).onclick = function() {
-                    sessionStorage.setItem('idPartida', `${json.games[i].id}`);
-                    sessionStorage.setItem('nombrePartida', `${json.games[i].name}`);
-                    window.location.href = "salaEquipos.html";
-                }
+            }
+            let listaBotones = document.querySelectorAll(".listaPartidas button");
+            for (let i = 0; i < listaBotones.length; i++) {
+                listaBotones[i].addEventListener('click', joinPartida);
             }
         }
     })
